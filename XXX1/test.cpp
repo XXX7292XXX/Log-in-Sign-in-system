a87556c02d0a4bdb195f66bedbe805af04469b86
XXX7292XXX <121560170+XXX7292XXX@users.noreply.github.com>
2023-03-16 15:54:24 +0300

    Update test.cpp

--- XXX1/test.cpp
+++ XXX1/test.cpp
@@ -1,64 +1,181 @@
 #include <iostream>
-#include <windows.h>
-#include <map>
-#include <fstream>
 #include <string>
+#include <fstream>
+#include <vector>
+
 using namespace std;
 
-class User
-{
-public:
-	auto UserData(string password, string login, map<string, string>(usermap), bool key) {
-		CheckData(login, password, map<string, string>(usermap), key); cout << key;
-	}
-private:
-	 void CheckData(string login, string password, map<string, string>(usermap),bool key) {
-		string s; ifstream file("userdata.txt");
-		while (getline(file, s)){
-			if (s == login + password) key = true;
-		}
-		if (key == true) { cout << "\n!Access is allowed!" << endl;}
-		else { cout << "\n!Acess is denied!" << endl; }
-	}
+const string FILE_NAME = "userdata.txt"; // Имя файла для сохранения данных пользователей
+const string ENCRYPTION_KEY = "mysecretkey"; // Ключ для шифрования данных
+
+// Функция для шифрования строки с помощью XOR
+string encrypt(const string& data) {
+    string result = data;
+    for (size_t i = 0; i < data.size(); ++i) {
+        result[i] = data[i] ^ ENCRYPTION_KEY[i % ENCRYPTION_KEY.size()];
+    }
+    return result;
+}
+
+// Функция для дешифрования строки с помощью XOR
+string decrypt(const string& data) {
+    return encrypt(data); // Для дешифрования используется та же операция, что и для шифрования
+}
+
+// Структура для хранения информации о пользователе
+struct User {
+    string name;
+    string email;
+    string password;
 };
 
-int main() {
-	bool start = true;bool key = false;
-	int a;
-	string login, password, newlogin, newpassword;
-	map <string, string> usermap;
-	setlocale(LC_ALL, "Rus");
-	User user1;
+// Функция для чтения данных о пользователях из файла
+void readUsersFromFile(vector<User>& users) {
+    ifstream file(FILE_NAME);
+    if (file.is_open()) {
+        while (true) {
+            User user;
+            getline(file, user.name);
+            getline(file, user.email);
+            getline(file, user.password);
+            if (file.fail()) break;
+            if (!user.name.empty()) {
+                user.name = decrypt(user.name);
+                user.email = decrypt(user.email);
+                user.password = decrypt(user.password);
+                users.push_back(user);
+            }
+        }
+        file.close();
+    }
+}
 
-	cout << "Good Day!!";
-	while (start) {
-		cout << "\nChoose one of the operations listed below!\n\n-Log in — 0.\n-Sign in — 1.\n-Quit — 2." << endl;
-		cin >> a;
-		switch(a) {
-			case 0:
-				{cout << "Login: "; cin >> login;
-				cout << "Password: "; cin >> password;
-				user1.UserData(password, login, map<string, string>(usermap), key);
-				break; }
-			case 1:
-				{ofstream fileuser("userdata.txt", ios::out | ios::app);
-				cout << "New Login: "; cin >> newlogin;
-				cout << "New Password: "; cin >> newpassword;
-				string s; ifstream file("userdata.txt"); bool createuser = true;
-				while (getline(file, s)) {
-					if ((s == newlogin + newpassword) or (s == newlogin)) createuser = false;
-				}
-				if (createuser == true) {
-					cout << "Your account has been successfully created! You can log in now." << endl;
-					usermap.insert(make_pair(newlogin, newpassword));
-					fileuser << newlogin + newpassword << endl; fileuser << newlogin << endl;
-				}
-				else { cout << "Account with the same name already exists!" << endl; }
-				break; }
-			default:
-				start = 0;
-		}
-	}
-	cout << "Programm finished!" << endl;
-	return 0;
+// Функция для записи данных о пользователях в файл
+void writeUsersToFile(const vector<User>& users) {
+    ofstream file(FILE_NAME);
+    if (file.is_open()) {
+        for (const auto& user : users) {
+            file << encrypt(user.name) << endl;
+            file << encrypt(user.email) << endl;
+            file << encrypt(user.password) << endl;
+        }
+        file.close();
+    }
+}
+
+// Функция для проверки, есть ли пользователь с указанным email и паролем
+bool isUserValid(const vector<User>& users, const string& email, const string& password) {
+    for (const auto& user : users) {
+        if (user.email == email && user.password == password) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Функция для получения индекса пользователя с указанным email и паролем
+int getUserIndex(const vector<User>& users, const string& email, const string& password) {
+    for (size_t i = 0; i < users.size(); ++i) {
+        if (users[i].email == email && users[i].password == password) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Функция для вывода информации о пользователе на экран
+void printUserInfo(const User& user) {
+    cout << "Name: " << user.name << endl;
+    cout << "Email: " << user.email << endl;
+    cout << "Password: " << user.password << endl;
+}
+
+// Функция для изменения информации о пользователе
+void changeUserInfo(User& user) {
+    cout << "Enter new name (leave empty to skip): ";
+    getline(cin, user.name);
+    if (!user.name.empty()) {
+        cout << "Enter new email: ";
+        getline(cin, user.email);
+        cout << "Enter new password: ";
+        getline(cin, user.password);
+        cout << "User info has been updated." << endl;
+    }
+}
+
+int main() {
+    vector<User> users;
+    readUsersFromFile(users);
+    while (true) {
+        cout << "Welcome to our program!" << endl;
+        cout << "1. Log in" << endl;
+        cout << "2. Register" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Your choice: ";
+        int choice;
+        cin >> choice;
+        cin.ignore(); // Игнорируем символ новой строки, оставшийся в потоке после ввода числа
+
+if (choice == 1) {
+            cout << "Enter your email: ";
+            string email;
+            getline(cin, email);
+            cout << "Enter your password: ";
+            string password;
+            getline(cin, password);
+            if (isUserValid(users, email, password)) {
+                cout << "You have been logged in successfully!" << endl;
+                int index = getUserIndex(users, email, password);
+                while (true) {
+                    cout << "1. View profile" << endl;
+                    cout << "2. Change profile info" << endl;
+                    cout << "3. Log out" << endl;
+                    cout << "Your choice: ";
+                    int choice;
+                    cin >> choice;
+                    cin.ignore();
+
+                    if (choice == 1) {
+                        printUserInfo(users[index]);
+                    }
+                    else if (choice == 2) {
+                        changeUserInfo(users[index]);
+                        writeUsersToFile(users);
+                    }
+                    else if (choice == 3) {
+                        cout << "You have been logged out." << endl;
+                        break;
+                    }
+                    else {
+                        cout << "Invalid choice." << endl;
+                    }
+                }
+            }
+            else {
+                cout << "Invalid email or password." << endl;
+            }
+        }
+        else if (choice == 2) {
+            cout << "Enter your name: ";
+            string name;
+            getline(cin, name);
+            cout << "Enter your email: ";
+            string email;
+            getline(cin, email);
+            cout << "Enter your password: ";
+            string password;
+            getline(cin, password);
+            users.push_back({ name, email, password });
+            writeUsersToFile(users);
+            cout << "You have been registered successfully!" << endl;
+        }
+        else if (choice == 3) {
+            break;
+        }
+        else {
+            cout << "Invalid choice." << endl;
+        }
+    }
+
+    return 0;
 }
